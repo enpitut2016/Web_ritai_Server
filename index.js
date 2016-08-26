d3ba7571d1e3a9cc4e5bb1171211d732fd8bbb63
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2016-08-26 11:33:54 +0900

    [add]パラメータのpref,cityから天気のurlを表示

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 var express = require('express');
 var app = express();
+var cheerio = require('cheerio-httpcli');
+var rootUrl = "http://www.tenki.jp";
 
 app.get('/', function (req, res) {
-  res.send('Hello, World!');
+    var pref = req.query.pref;
+    var city = req.query.city;
+
+    res.send(getLocationPage(pref, city));
 });
+
+function getPrefPage(pref) {
+    var prefPageUrl;
+    var prefSc = cheerio.fetchSync(rootUrl);
+    prefSc.$('ul.localList a').each(function(tag){
+	if(prefSc.$(this).text() == pref){
+	    prefPageUrl = prefSc.$(this).attr('href');
+	}
+    });
+    return rootUrl + prefPageUrl;
+}
+
+function getCityPage(prefPageUrl, city) {
+    var cityPageUrl;
+    var citySc = cheerio.fetchSync(prefPageUrl);
+    citySc.$('.weatherWaveBox ul li a').each(function(tag){
+	if(citySc.$(this).text() == city){
+	    cityPageUrl = citySc.$(this).attr('href');
+	}
+    });
+    return rootUrl + cityPageUrl;
+}
+
+function getLocationPage(pref, city){
+    var prefPageUrl = getPrefPage(pref);
+    var cityPageUrl = getCityPage(prefPageUrl, city);
+    return cityPageUrl;
+}
+
+function getWeatherData(url) {
+    url = 'http://www.tenki.jp/forecast/3/11/4020/8220-daily.html';
+    cheerio.fetch(url, function(err, $, res){
+	console.log(res.headers);
+    });
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
